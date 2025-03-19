1d72b93e93acb82453cbc8a32d53643f55277dd5
Maximus1 <1571469+Maximus1@users.noreply.github.com>
2025-03-19 15:20:09 +0100

    Änderungen: -Müll entsorgt

--- Tests/Video Transcoder.Tests.ps1
+++
@@ -1,8 +0,0 @@
-Describe 'Video Transcoder Tests' {
-    It 'should return the correct output for a given input' {
-        $input = 'input.mp4'
-        $expectedOutput = 'output.mp4'
-        $actualOutput = Invoke-VideoTranscoder -InputFile $input
-        $actualOutput | Should -BeExactly $expectedOutput
-    }
-}
